b0ecbc1d7ca01cfd34910d6820de61e541f5c0fa
Marcin Mańk <m.mank@m-t.pl>
2015-03-27 17:48:52 +0100

    disable broken library checks. They explode on my MAC

--- ext/tiny_tds/extconf.rb
+++ ext/tiny_tds/extconf.rb
@@ -91,11 +91,11 @@ def asplode(lib)
   abort "-----\n#{lib} is missing.\n-----"
 end
 
-asplode 'libiconv' unless have_func('iconv_open', 'iconv.h') || have_library('iconv', 'iconv_open', 'iconv.h')
-asplode 'freetds'  unless have_header('sybfront.h') && have_header('sybdb.h')
+#asplode 'libiconv' unless have_func('iconv_open', 'iconv.h') || have_library('iconv', 'iconv_open', 'iconv.h')
+#asplode 'freetds'  unless have_header('sybfront.h') && have_header('sybdb.h')
 
-asplode 'freetds'  unless find_library("#{lib_prefix}sybdb", 'tdsdbopen')
-asplode 'freetds'  unless find_library("#{lib_prefix}ct",    'ct_bind')
+#asplode 'freetds'  unless find_library("#{lib_prefix}sybdb", 'tdsdbopen')
+#asplode 'freetds'  unless find_library("#{lib_prefix}ct",    'ct_bind')
 
 create_makefile('tiny_tds/tiny_tds')
 
